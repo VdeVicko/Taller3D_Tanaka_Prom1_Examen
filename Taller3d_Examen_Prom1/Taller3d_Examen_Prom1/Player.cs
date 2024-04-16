e5dc4d73240aecacdfe0720f114505c63184760b
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 17:25:21 -0500

    Player.cs constructor

---
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller3d_Examen_Prom1
+{
+    class Player : Entity
+    {
+        public Player(string name,float life, float Strg, float Cha, float Dex) : base(name,life )
+        {
+
+        }
+    }
+}
