a06261b982c3a3915843479246112188b8c7812b
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 17:19:36 -0500

    Add stats to Entity - Victor S

--- Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Entity.cs
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Entity.cs
@@ -10,10 +10,19 @@ namespace Taller3d_Examen_Prom1
 
         protected string name { get; set; }
 
-        public Entity(string name, float life)
+        protected float strg { get; set; }
+
+        protected float dex { get; set; }
+
+        protected float cha { get; set; }
+
+        public Entity(string name,float life, float strg,float dex, float cha )
         {
             this.life = life;
             this.name = name;
+            this.strg = strg;
+            this.dex = dex;
+            this.cha = cha;
         }
 
     }
