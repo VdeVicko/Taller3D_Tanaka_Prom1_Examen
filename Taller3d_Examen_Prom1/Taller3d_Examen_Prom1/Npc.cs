15844d4994575c0dee0f13e882efe4d723bccd16
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 17:50:13 -0500

    Function Interact in npc - Victor S

--- Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Npc.cs
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Npc.cs
@@ -6,9 +6,16 @@ namespace Taller3d_Examen_Prom1
 {
     class Npc : Entity
     {
+        
+
         public Npc(string name, float life, int strg, int dex, int cha) : base(name, life, strg, dex, cha)
         {
 
+        }
+
+        public virtual void Interact()
+        {
+            
         }
 
 
