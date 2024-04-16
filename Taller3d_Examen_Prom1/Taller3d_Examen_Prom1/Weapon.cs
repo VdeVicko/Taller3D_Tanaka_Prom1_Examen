dfae9e2f34000e182bb00868cbde7288d507c80c
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 18:16:13 -0500

    Create Weapon class

---
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller3d_Examen_Prom1
+{
+    class Weapon : Item
+    {
+        float dmg { get; set; }
+        public Weapon(string name,float coast) : base(name,coast)
+        {
+
+        }
+    }
+}
