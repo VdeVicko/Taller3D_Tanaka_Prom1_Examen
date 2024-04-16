2e12d38cb658ec716ff312c8ba2ed62c2da4b077
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 16:34:14 -0500

    Create the VisualStudio project

---
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taller3d_Examen_Prom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
