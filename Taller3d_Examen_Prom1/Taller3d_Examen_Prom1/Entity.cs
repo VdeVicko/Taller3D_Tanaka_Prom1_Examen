b4ee2833fcb2fd2bf60e988dd56ee42f123337ac
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 16:53:28 -0500

    Class Entity - will be the father or class Npcs and Player

---
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller3d_Examen_Prom1
+{
+    class Entity
+    {
+        protected float life { get; set; }
+
+        protected string name { get; set; }
+
+    }
+
+}
