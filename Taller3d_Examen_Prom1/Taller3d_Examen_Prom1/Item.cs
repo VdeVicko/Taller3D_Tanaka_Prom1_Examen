048ab8b1300878edbc33d0fdd38a9b56ae5f3a74
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 18:08:57 -0500

    Create item class

---
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller3d_Examen_Prom1
+{
+    class Item
+    {
+        public float coast { get; set; }
+        public float name { get; set; }
+        public Item(float cost)
+        {
+
+        }
+
+
+    }
+}
