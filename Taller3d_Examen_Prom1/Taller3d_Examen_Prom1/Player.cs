04ea08f6fb39b37a16458e85209546070fb7133e
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 17:42:36 -0500

    Squashed commit of the following:
    commit 835adc740cacf362cee0b0c2bbbdebd928d53bcf
    Author: Victor  Sanchez <sanchezvs2004@gmail.com>
    Date:   Tue Apr 16 17:41:28 2024 -0500
    
        change float for int

--- Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Player.cs
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Player.cs
@@ -6,9 +6,18 @@ namespace Taller3d_Examen_Prom1
 {
     class Player : Entity
     {
-        public Player(string name,float life, float Strg, float Cha, float Dex) : base(name,life )
+
+        public Player(string name,float life, int strg, int dex, int cha) : base(name,life, strg,dex,cha )
         {
 
         }
+
+        public void UseInstrument()
+        {
+            Random rand = new Random();
+            dex = rand.Next(1, dex);
+        }
+
+       
     }
 }
