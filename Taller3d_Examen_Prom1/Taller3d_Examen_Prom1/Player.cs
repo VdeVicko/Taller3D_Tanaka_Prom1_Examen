24d6960bdad538a8d235fe59969a579fcbec49a5
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 21:50:49 -0500

    Squashed commit of the following:
    commit ea4c6a6cd7ad3c643a80d810186a0f8954e2c153
    Author: Victor  Sanchez <sanchezvs2004@gmail.com>
    Date:   Tue Apr 16 18:55:03 2024 -0500
    
        Add Function Addpoints to player class

--- Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Player.cs
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/Player.cs
@@ -12,6 +12,26 @@ namespace Taller3d_Examen_Prom1
 
         }
 
+        public void AddPoints(int point)
+        {
+            Console.WriteLine("Select the stat");
+            Console.WriteLine("1.Strg: " + strg);
+            Console.WriteLine("2.Dex: " + dex);
+            Console.WriteLine("3.Cha " + cha);
+            switch(int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    strg += point;
+                    break;
+                case 2:
+                    dex += point;
+                    break;
+                case 3:
+                    cha += point;
+                    break;
+            }
+        }
+
         public void UseInstrument()
         {
             Random rand = new Random();
