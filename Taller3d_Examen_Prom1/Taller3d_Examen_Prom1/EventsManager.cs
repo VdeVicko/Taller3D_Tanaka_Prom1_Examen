ca517b5050d3170279663c47f3a0e64dd708c423
Victor  Sanchez <sanchezvs2004@gmail.com>
2024-04-16 19:15:26 -0500

    Create player

---
+++ Taller3d_Examen_Prom1/Taller3d_Examen_Prom1/EventsManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller3d_Examen_Prom1
+{
+    class EventsManager
+    {
+        List<Player> playableCharacters = new List<Player>();
+        List<NpcFriendly> npcs = new List<NpcFriendly>();
+
+        public void CreatePlayer()
+        {
+            int points = 10;
+            Player player = new Player("null",0,0,0,0);
+            Console.WriteLine("Your character has 3 stats,they help you to succes the events, you can add points to your stats");
+            do
+            {
+                player.AddPoints(points);
+
+            }while (points >= 0);
+
+            Console.WriteLine("Whats the name of the character");
+            player.Name = Console.ReadLine();
+            playableCharacters.Add(player);
+        }
+
+       
+
+    }
+}
